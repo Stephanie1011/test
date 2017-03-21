b4123dabe9a0fa46c74b00be3c1a4c65c2d147a5
Stephanie <chang.s.chunning@gmail.com>
2017-03-20 22:11:59 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+﻿# -*- coding=UTF-8 -*-
+if __name__ == '__main__':
+	f = open("test.txt","r", encoding="UTF-8")
+	b = f.readline().strip()
+	a = "鹤"
+	if a==b:
+		print("good1")
+	b = f.readline().strip()
+	c = "鶴"
+	if (b==c):
+		print("good2")
+	f.close()
